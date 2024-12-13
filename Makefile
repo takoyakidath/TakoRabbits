f441edbe6baf0ddf30516a25ed69287483a5a4b0
TakoyakiDayo <Takoyaki1051@outlook.jp>
2024-12-13 22:32:04 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -o TakoRabbit src/main.cpp
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // アスキーアートの表示
+    std::cout << " /\\_/\\  \n";
+    std::cout << "( o.o ) \n";
+    std::cout << " > ^ <  \n";
+
+    return 0;
+}
